01cc018d0cacf95a45558781769b8b20554040ea
JinSeok Oh <jinseok0.oh@samsung.com>
2023-07-05 15:09:27 +0900

    Simple Factor Commit

--- Project10/prime-factors.cpp
+++ Project10/prime-factors.cpp
@@ -1,4 +1,15 @@
+#include <vector>
+using namespace std;
 class PrimeFactor
 {
-	
+public:
+	vector<int> of(int number)
+	{
+		vector<int> result = {};
+		if (number == 2)
+		{
+			result.push_back(2);
+		}
+		return result;
+	}
 };
--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -1,7 +1,22 @@
 #include "pch.h"
 #include "../Project10/prime-factors.cpp"
+#include <vector>
+
+using namespace std;
 
 TEST(PrimeFactors, PrimeTest) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
+}
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
